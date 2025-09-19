8111273f270123165a947337b6284f8c491d4267
JereMaya22 <jeremy.rivera409@gmail.com>
2025-09-19 17:47:35 -0600

    hoy si se agrego el productdto

--- src/main/java/shareddtos/billmodule/ProductBillDTO.java
+++ src/main/java/shareddtos/billmodule/ProductBillDTO.java
@@ -1,5 +1,15 @@
 package shareddtos.billmodule;
 
-public class ProductBillDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductBillDTO {
+    private Long id;
+    private String name;
+    private Integer quantity;
+    private Double price;
 }
