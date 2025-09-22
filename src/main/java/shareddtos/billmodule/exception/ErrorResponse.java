339fef032f72035e5df958c7a2eef7d3ea33e956
JereMaya22 <jeremy.rivera409@gmail.com>
2025-09-22 11:11:01 -0600

    added errorResponse

---
+++ src/main/java/shareddtos/billmodule/exception/ErrorResponse.java
@@ -0,0 +1,22 @@
+package shareddtos.billmodule.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class ErrorResponse {
+
+    private LocalDateTime timestemp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
+    private List<String> details;
+
+}
